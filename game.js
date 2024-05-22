39604ed09ae6763acc17cda14d9d8fae21887832
TeamZebra <thezebra39@gmail.com>
2024-05-22 23:44:17 +0100

    Added a function to play 5 rounds and keep track records of scores

--- game.js
+++ game.js
@@ -14,12 +14,35 @@ function getComputerChoice() {
     return computerSelection[randomIndex];
 }
 
+// Function to play 5 rounds and declare and keep track of scores
 function playRound() {
     
     let humanScore = 0;
     let computerScore = 0;
     let humanSelection;
     let computerSelection;
+
+    while (humanScore < 5 && computerScore < 5) {
+        let humanWinCount = ((`YOU (${humanSelection}) WIN, Computer (${computerSelection}) lost.`));
+        let computerWinCount = ((`YOU (${humanSelection}) lost, Computer (${computerSelection}) won.`));
+
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+
+    if (humanSelection === computerSelection) {
+        console.log(`YOU picked (${humanSelection}), and COMPUTER picked (${computerSelection}), IT'S A TIE!`);
+    } else if (humanSelection === 'rock' && (computerSelection === 'paper' || computerSelection === 'scissors') || (humanSelection === 'scissors' && computerSelection === 'paper')) {
+        console.log(humanWinCount);
+        humanScore++;
+        console.log(`Your score: ${humanScore}\nComputer score: ${computerScore}`);
+    } else if ((humanSelection === 'scissors' && computerSelection === 'rock') || (humanSelection === 'paper' && (computerSelection === 'scissors' || computerSelection === 'rock'))) {
+        console.log(computerWinCount);
+        computerScore++;
+        console.log(`Your score: ${humanScore}\nComputer score: ${computerScore}`);
+    } else {
+        console.log('you entered wrongly!');
+    }
+}
 
 
 

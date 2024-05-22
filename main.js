00ae8e7147ab60c63007df8ce02412be44f49f5f
TeamZebra <thezebra39@gmail.com>
2024-05-22 14:10:51 +0100

    Added game rules and regulations

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     return computerSelection[randomIndex];
 }
+
+// game rules & regulation
+function playRound(humanSelection, computerSelection) {
+    if (humanSelection === computerSelection) {
+        console.log(`YOU picked (${humanSelection}), and COMPUTER picked (${computerSelection}), IT'S A TIE!`);
+    } else if (humanSelection === 'rock' && (computerSelection === 'paper' || computerSelection === 'scissors') || (humanSelection === 'scissors' && computerSelection === 'paper')) {
+        console.log(humanWinCount);
+        return "humanWon";
+    } else if ((humanSelection === 'scissors' && computerSelection === 'rock') || (humanSelection === 'paper' && (computerSelection === 'scissors' || computerSelection === 'rock'))) {
+        console.log(computerWinCount);
+        return "computerWon";
+    } else {
+        console.log('you entered wrongly!');
+    }
+}
